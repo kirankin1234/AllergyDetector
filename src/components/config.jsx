f8ffa8098faaf4ee97158bec684dd6066c81a8db
Kiran Gaikwad <kiran899964@gmail.com>
2025-12-16 23:58:18 +0530

    hi

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,2 +1,3 @@
 // src/config.js
-export const API_BASE_URL = 'http://localhost:8000/api';
+// export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'https://allergydetectorbackend-2.onrender.com/api';
